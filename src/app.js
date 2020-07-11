0288714feb0f77e9b28a616434ae59b493863b70
YERIM <dnaer456@gmail.com>
2020-07-11 13:33:48 +0900

    create pug

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ const PORT = 3000;
 // server 시작점에 views 폴더를 찾음
 app.set("view engine", "pug");
 
+app.get("/", (req, res) => {
+  res.render("main");
+});
+
 app.listen(PORT, () => {
   console.log(`✅  ${PORT}Server Start :: City GONG-JU Description SPA`);
 });
